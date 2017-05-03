4084208e59cd3f7cb7e2f4a2b69e66c2bc3ffe33
Deepak.c <cse15b016@iiitk.ac.in>
2017-05-03 23:06:30 +0530

    Added splash screen with Axicut video

--- app/src/main/java/com/example/dingu/axicut/MainActivity.java
+++ app/src/main/java/com/example/dingu/axicut/MainActivity.java
@@ -1,29 +1,51 @@
 package com.example.dingu.axicut;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.VideoView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }, TIME_OUT);
+        try{
+            VideoView videoHolder = (VideoView)findViewById(R.id.SplashVideo);
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/"
+                    + R.raw.axicut_splash_vid);
+            videoHolder.setVideoURI(video);
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    jump();
+                }
+            });
+            videoHolder.start();
+        }catch (Exception e){
+            jump();
+        }
+    }
+    private void jump(){
+        if(isFinishing())
+            return;
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        jump();
+        return true;
+
+    }
 }
