975f3dc5b93861907b8c0b1f10a3bd5b1c6a89ec
Deepak.c <cse15b016@iiitk.ac.in>
2017-05-22 19:46:54 +0530

    Added a timer for production fragment

--- app/src/main/java/com/example/dingu/axicut/Production/ProdTimer.java
+++ app/src/main/java/com/example/dingu/axicut/Production/ProdTimer.java
@@ -1,12 +1,18 @@
 package com.example.dingu.axicut.Production;
 
 
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.dingu.axicut.R;
 
@@ -15,6 +21,18 @@ import com.example.dingu.axicut.R;
  */
 public class ProdTimer extends DialogFragment {
 
+    private ImageButton startButton;
+    private ImageButton pauseButton;
+    private ImageButton stopButton;
+    private TextView timerValue;
+
+    private long startTime = 0L;
+
+    private Handler customHandler = new Handler();
+
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
 
     public ProdTimer() {
         // Required empty public constructor
@@ -27,5 +45,44 @@ public class ProdTimer extends DialogFragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_prod_timer, container, false);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        startButton=(ImageButton) getView().findViewById(R.id.startButton);
+        pauseButton=(ImageButton)getView().findViewById(R.id.pauseButton);
+        stopButton=(ImageButton)getView().findViewById(R.id.stopButton);
+        timerValue=(TextView)getView().findViewById(R.id.timertext);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+            }
+        });
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            timerValue.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+
+    };
 
 }
