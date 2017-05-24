fb1f1a0e4c81483b279ac0d06ef9fd1956d233e7
Deepak.c <cse15b016@iiitk.ac.in>
2017-05-24 10:55:04 +0530

    Implemented a singleton pattern for the tempAuth creator.

--- app/src/main/java/com/example/dingu/axicut/Utils/General/MyDatabase.java
+++ app/src/main/java/com/example/dingu/axicut/Utils/General/MyDatabase.java
@@ -1,5 +1,9 @@
 package com.example.dingu.axicut.Utils.General;
 
+import android.content.Context;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
 /**
@@ -8,12 +12,27 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MyDatabase {
     private static FirebaseDatabase mDatabase;
-
+    private MyDatabase(){};
     public static FirebaseDatabase getDatabase() {
         if (mDatabase == null) {
             mDatabase = FirebaseDatabase.getInstance();
             mDatabase.setPersistenceEnabled(true);
         }
         return mDatabase;
+    }
+    public static class TempAuthCreator{
+      private static FirebaseOptions firebaseOptions;
+        private static FirebaseApp myApp;
+        private  TempAuthCreator() {}
+        public static FirebaseApp getTempAuth(Context context){
+            if(myApp==null){
+                firebaseOptions = new FirebaseOptions.Builder()
+                        .setDatabaseUrl("https://axicut-3fe9b.firebaseio.com/")
+                        .setApiKey("AIzaSyDKjXmCJ377LALkI87aIX8fa9Km-_OcF68")
+                        .setApplicationId("axicut-3fe9b").build();
+                myApp = FirebaseApp.initializeApp(context, firebaseOptions, "axicut");
+            }
+            return myApp;
+        }
     }
 }
